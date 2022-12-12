61f26220581e10db3319a087c044d72df5f6acc4
Jazmine <lm28810@gmail.com>
2022-12-12 07:02:45 -0500

    matching  logic

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,72 @@
+// starting parameters
+// Cards Array, timer
+
+let cards = document.querySelectorAll('.card')
+let t = document.getElementById('time-remaining');
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+// console.log(cards)
 
 
-// Main Function
-let ready = () => {
-    let overlays = document.querySelectorAll('.overlay-text')
-    let cards = document.querySelectorAll(".card")
+// shuffle cards
+    
+function shuffle() {
+  cards.forEach(card => {
+    let randomCard = Math.floor(Math.random() * cards.length);
+    card.style.order = randomCard;
+  });
+}
+shuffle();
 
-    // Making sure overlays and cards are in arrays
-    console.log(overlays)
-    console.log(cards)
 
-    // Add click events
+// start count down
+let timer = () => {
+    let countDown = setInterval(() => {
+        
+        t.innerText--;
+        if (t.innerText <= 0) {
+            clearInterval(countDown);
+            document.getElementById('game-over-text').classList.add('visible');
+        }
+    }, 1000);
+
+   }
+ 
+    
+// remove overlay & start Game 
+// make visible class disappear
+
+const gamestart = () => {
+    let overlays = document.querySelectorAll('.overlay-text');
     overlays.forEach(overlay => {
-        // this will make the overlay disappear at game start
-        overlay.addEventListener('click',()=>{overlay.classList.remove('visible')})
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove('visible');
+            // delay timer until after overlay disappears
+            setTimeout(timer, 2000);
+            // reset timer
+            t.innerText = 10
+            // reset flips
+            // reset matches
+        })
     })
-    //start game 
+
 }
 
-// Calling Main Function
-ready()
+gamestart()
+
+
+
+const flip = () => {
+    cards.forEach(card=>{card.addEventListener('click',()=> card.classList.add('visible') )})
+}
+
+flip()
+
+// const gameOver = ()=> {
+//     clearInterval(countDown);
+//     document.getElementById('game-over-text').classList.add('visible');
+    
+// }
+//     gameOver()
